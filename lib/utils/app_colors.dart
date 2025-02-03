202014fc99e6cf9f769b73fca03af62ffab610e7
DavidRaveloU <dravelo@unicesar.edu.co>
2025-02-03 00:01:32 -0500

    Add AppColors utility class for centralized color management

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF0062D1);
+  static const Color primaryVariant = Color(0xFF1B3B78);
+  static const Color secondary = Color(0xFFD16F00);
+  static const Color secondaryVariant = Color(0xFFA4580B);
+
+  static const Color title = Color(0xFF2D2D2D);
+  static const Color subtitle = Color(0xFF767676);
+  static const Color subtitle2 = Color(0XFFC2C2C2);
+}
